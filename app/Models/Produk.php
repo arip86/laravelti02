d11b18921ab1e8b0cd049505c44801d57e0bc610
arip86 <akhmadarip86@gmail.com>
2023-06-06 16:58:18 +0700

    laravel

---
+++ app/Models/Produk.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+    use HasFactory;
+    //panggil table produk 
+    protected $table = 'produk';
+    protected $fillable = [
+        'kode',
+        'nama',
+        'harga_jual',
+        'harga_beli',
+        'stok',
+        'min_stok',
+        'deskripsi',
+        'kategori_produk_id'
+    ];
+    //relasi one To Many ke table kategori produk
+    public function kategoriProduk(){
+        return $this->belongsTo(kategoriProduk::class);
+    }
+}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layout.appadmin')
+@section('content')
+<h1>Ini adalah Dashboard</h1>
+
+@endsection
